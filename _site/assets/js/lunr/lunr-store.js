451634c3601510986362acfe1ffad52f57e7b505
Lance <1572534835@qq.com>
2020-11-21 01:16:57 +0800

    feat: 更新第二篇文章

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "前端vscode插件配置",
+        "excerpt":"外观配置 主题：Chester Atom 图标：VSCode Great Icons 字体：Fira Code 预览 用户设置（User Settings） { \"editor.fontSize\": 16, \"workbench.colorTheme\": \"Chester Atom\", \"editor.fontFamily\": \"Fira Code\", \"emmet.triggerExpansionOnTab\": true, \"editor.fontLigatures\": true } 风格检查 ESLint StyleLint MarkdownLint Prettier 编码效率 代码片段 HTML Snippets Javascript (ES6) Code Snippets Javascript Patterns Snippets Document This，一键给代码中的类、函数加上注释，支持函数声明、函数表达式、箭头函数等； 自动补全 Auto Close Tag，在打开标签并且键入 &lt;/ 的时候，能自动补全要闭合的标签； Auto...","categories": ["IDE"],
+        "tags": ["VSCode"],
+        "url": "http://localhost:4000/ide/%E5%89%8D%E7%AB%AFvscode%E6%8F%92%E4%BB%B6%E9%85%8D%E7%BD%AE/",
+        "teaser": null
+      },{
+        "title": "使用hexo搭建个人博客",
+        "excerpt":"categories: 博客 tags: Hexo — 介绍 利用 Hexo 在 GitHub 上搭建个人博客。 准备工作 你的电脑需要安装下面两个应用程序： NodeJS 如何安装 Git 如何安装 重要步骤 安装Hexo 安装文档 插件安装 hexo-abbrlink 介绍 &amp;&amp; 此插件诞生原因 hexo-deployer-git 将本地博客部署到GitHub 如何安装和使用 hexo-server npm install hexo-server –save 服务器模块。在启动期间，Hexo 会监视文件变动并自动更新 使用第三方主题 Next Yilia 创建分类功能 运行： hexo new page \"categories\" p.s. 命名为categories原因是 categories 在next主题中是一个关键词对应着分类。 在主站根目录下,也就是你hexo的目录下找到 /source/categories/index.md，打开后的效果类似下方代码：...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/%E4%BD%BF%E7%94%A8hexo%E6%90%AD%E5%BB%BA%E4%B8%AA%E4%BA%BA%E5%8D%9A%E5%AE%A2/",
+        "teaser": null
+      }]
