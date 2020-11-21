0e3ac2a20791c62dbbf12b6f4c1031b29687155d
Lance <1572534835@qq.com>
2020-11-21 11:39:14 +0800

    feat: 文章大纲

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/%E4%BD%BF%E7%94%A8hexo%E6%90%AD%E5%BB%BA%E4%B8%AA%E4%BA%BA%E5%8D%9A%E5%AE%A2/",
         "teaser": null
+      },{
+        "title": "对html语义化的理解",
+        "excerpt":"对HTML语义化的理解 核心：用正确的标签做正确的事情 利与开发：方便代码的阅读和维护 利于SEO：方便爬虫根据 语义标签 确定 页面结构 和 关键字 的权重 常见语义化标签 一图胜千言： 元素细节： ](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/main) 存放每个页面独有的内容。每个页面上只能用一次 `&lt;main&gt;`，且直位于 [中。最好不要把它嵌套进其它元素。 `` 包围的内容即一篇文章，与页面其它部分无关（比如一篇博文）。 `` 与 &lt;article&gt; 类似，但 &lt;section&gt; 更适用于组织页面使其按功能（比如迷你地图、一组文章标题和摘要）分块。一般的最佳用法是：以 标题 作为开头；也可以把一篇 &lt;article&gt; 分成若干部分并分别置于不同的 &lt;section&gt; 中，也可以把一个区段 &lt;section&gt; 分成若干部分并分别置于不同的 &lt;article&gt; 中，取决于上下文。 `` 包含一些间接信息（术语条目、作者简介、相关链接，等等）。 ](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/header) 是简介形式的内容。如果它是 [ 的子元素，那么就是网站的全局页眉。如果它是 ](https://developer.mozilla.org/zh-CN/docs/Web/HTML/Element/article) 或[ 的子元素，那么它是这些部分特有的页眉（此 &lt;header&gt; 非彼 标题）。 `` 包含页面主导航功能。其中不应包含二级链接等内容。 ``...","categories": ["HTML"],
+        "tags": [],
+        "url": "http://localhost:4000/html/%E5%AF%B9HTML%E8%AF%AD%E4%B9%89%E5%8C%96%E7%9A%84%E7%90%86%E8%A7%A3/",
+        "teaser": null
       }]
